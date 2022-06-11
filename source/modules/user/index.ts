33efeff744178ae19c65ea216f192c368f049c9b
SHAJAL987 <ashiqulislamshajal.cse@gmail.com>
2022-06-11 07:27:49 -0700

    test

--- source/modules/user/index.ts
+++ source/modules/user/index.ts
@@ -1,4 +1,4 @@
-import set_userRouter from './routers/sampleRouter';
+import set_userRouter from './routers/sample.route';
 
 function init(app: any) {
     app.use('/sample', set_userRouter);
--- source/modules/user/routers/sampleRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import sampleService from '../services/sampleService';
-
-const router = express.Router();
-
-router.get('/pong', sampleService.sampleHealthCheck);
-
-export default router;
